cdad14dc6a0d6c5ac5ddf30cddc2a1edecd660e3
DusekDan <dusekdan@gmail.com>
2017-03-31 18:24:26 +0200

    Relocated data type definitions and corresponding functions

---
+++ Types.hs
@@ -0,0 +1,60 @@
+module Types where
+
+    import Data.List (intercalate)
+
+    -- Arguments data structures
+    data Action 
+      = Represent 
+      | Transform 
+
+    data Arguments = Arguments
+      { action :: Action 
+      , path :: String
+      }
+
+
+    -- Regex data structures
+    data Tree = BinaryOperation Char Tree Tree | Character Char | Star Char Tree deriving Show -- jmena operací místo současných konstruktorů
+    type Stack = [Tree]
+
+
+    -- Finite State Machine structures
+    -- For fast debug in ghci: FSM [1,2] ['a','b'] [TTransition 1 'a' 2, TTransition 2 'b' 3] 1 [2]
+    data FSM = FSM
+        {   states :: [TState] 
+        ,   alphabet :: [Char]                  -- Actually is not neccessary, project specification does not require to store this
+        ,   transitions :: [TTransition]  
+        ,   initialState :: TState
+        ,   finalStates :: [TState]
+        } 
+
+    type TState = Int
+    
+    data TTransition = TTransition
+        {   from :: TState
+        ,   symbol :: TransitionLabel
+        ,   to :: TState
+        }
+
+    data TransitionLabel 
+      = TransitionLabel Char 
+      | Epsilon
+    
+    -- Finite State Machine structures show overloads
+    instance Show TTransition where
+        show (TTransition f s t) = show f ++ "," ++  show s ++ "," ++ show t
+
+    instance Show TransitionLabel where
+        show (TransitionLabel c) = [c]
+        show (Epsilon) = ""
+ 
+    instance Show FSM where
+        show (FSM s _ t i f) = unlines  [displayStates s, displayInitialState, displayFinalStates f, displayTransitions t]
+            where
+                displayStates = intercalate "," . map show   -- ETA reduced from:  displayStates s = intercalate ","  (map show s)  
+                displayInitialState = show i
+                displayFinalStates = intercalate "," . map show -- ETA reduced from: displayFinalStates f = ... 
+                displayTransitions = intercalate "\n" . map show -- ETA reduced as all of those above
+
+    -- Other usefull constant functions 
+    regexNotValid = "Invalid Input"
